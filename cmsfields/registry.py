55f0cd51c7f057ff0d8296030181eab02e615bab
Diederik van der Boor <vdboor@edoburu.nl>
2012-06-01 00:24:35 +0200

    Fix ContentType error at first syncdb

--- cmsfields/registry.py
+++ cmsfields/registry.py
@@ -1,5 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
 from django import forms
+from django.db.utils import DatabaseError
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -46,7 +47,11 @@ class UrlTypeRegistry(object):
         if any(urltype.model == ModelClass for urltype in self._url_types):
             raise ValueError("Model is already registered: '{0}'".format(ModelClass))
 
-        ct = ContentType.objects.get_for_model(ModelClass)
+        try:
+            ct = ContentType.objects.get_for_model(ModelClass)
+        except DatabaseError:
+            return   # skip at first syncdb
+
         if not prefix:
             # Store something descriptive, easier to lookup from raw database content.
             prefix = '{0}.{1}'.format(*ct.natural_key())
