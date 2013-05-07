1a037ca1efb1d15c34063369353aa6bef1602c05
Diederik van der Boor <vdboor@edoburu.nl>
2013-05-07 17:44:20 +0200

    Bump version number, move version to any_urlfield/__init__.py

--- any_urlfield/__init__.py
+++ any_urlfield/__init__.py
@@ -1,1 +1,2 @@
-VERSION = (1,0,4)
+# following PEP 386
+__version__ = "1.0.5"
--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-from os.path import dirname, join
-import sys, os
+from os import path
+import codecs
+import os
+import re
+import sys
 
 # When creating the sdist, make sure the django.mo file also exists:
 if 'sdist' in sys.argv:
@@ -13,9 +16,22 @@ if 'sdist' in sys.argv:
         os.chdir('..')
 
 
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name='django-any-urlfield',
-    version='1.0.4',
+    version=find_version('any_urlfield', '__init__.py'),
     license='Apache License, Version 2.0',
 
     requires=[
@@ -23,7 +39,7 @@ setup(
     ],
 
     description='An improved URL selector to choose between internal models and external URLs',
-    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    long_description=read('README.rst'),
 
     author='Diederik van der Boor',
     author_email='opensource@edoburu.nl',
