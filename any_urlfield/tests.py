7f25a8d4850c7712d92cbc1f94e476e771b857e3
Diederik van der Boor <vdboor@edoburu.nl>
2015-04-15 14:33:22 +0200

    Added loaddata/dumpdata tests

--- any_urlfield/tests.py
+++ any_urlfield/tests.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
+import json
 import pickle
+from django.core import serializers
+from django.core.serializers.base import DeserializationError
 from django.db import models
 from django.test import TestCase
 from any_urlfield.models import AnyUrlField, AnyUrlValue
@@ -169,3 +172,43 @@ class AnyUrlTests(TestCase):
         obj = UrlModel(url=AnyUrlValue.from_db_value('http://www.example.org/'))
         obj.save()
         self.assertTrue(obj.pk)
+
+
+    def test_dumpdata(self):
+        """
+        See if the dumpdata routines handle the value properly.
+        """
+        page2 = RegPageModel.objects.create(slug='foo2')
+        UrlModel.objects.create(
+            url=AnyUrlValue.from_model(page2)
+        )
+
+        json_data = serializers.serialize("json", UrlModel.objects.all())
+        data = json.loads(json_data)
+        self.assertEqual(data, [{"fields": {"url": "any_urlfield.regpagemodel://1"}, "model": "any_urlfield.urlmodel", "pk": 1}])
+
+
+    def test_loaddata(self):
+        """
+        See if the loaddata routines handle the value properly.
+        """
+        fixture = '[{"fields": {"url": "any_urlfield.regpagemodel://999999"}, "model": "any_urlfield.urlmodel", "pk": 9}]'
+
+        objects = serializers.deserialize('json', fixture)
+        for deserializedobject in objects:
+            deserializedobject.save()
+
+        obj = UrlModel.objects.get(pk=9)
+        value = obj.url
+        self.assertEqual(value.type_prefix, 'any_urlfield.regpagemodel')
+        self.assertEqual(value.type_value, 999999)
+
+
+    def test_loaddata_exception(self):
+        """
+        Passing invalid values is simply not working.
+        """
+        fixture = '[{"fields": {"url": "unknown.unknown://999999"}, "model": "any_urlfield.urlmodel", "pk": 9}]'
+
+        objects = serializers.deserialize('json', fixture)
+        self.assertRaises(DeserializationError, lambda: list(objects))
